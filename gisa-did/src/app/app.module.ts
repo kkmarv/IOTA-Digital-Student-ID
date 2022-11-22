2145f569eced331c83718a1407687793f1781112
styagill <yannisleander.gille@student.hs-anhalt.de>
2022-11-22 23:30:52 +0100

    add api call

--- gisa-did/src/app/app.module.ts
+++ gisa-did/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -30,6 +31,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
--- gisa-did/src/app/login-page/login-page.component.ts
+++ gisa-did/src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-page',
@@ -7,13 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  log(kram: any) {
-    console.log(kram);
+  file: any;
+
+  changeFile(e: any) {
+    this.file = e.target.files[0];
   }
 
+  uploadFile() {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      console.log(fileReader.result);
+      this.http.post("localhost:8080", fileReader.result?.toString);
+    }
+    fileReader.readAsText(this.file);
+  }
+
+  log(inputEvent: any) {
+    let file: File = inputEvent.target.files[0]
+    console.log(file);
+    let fileReader = new FileReader();
+    fileReader.onload?(() => {
+      console.log(fileReader.result);
+      console.error("lol");
+    }):
+    fileReader.readAsText(file);
+    console.log(fileReader.result);
+  }
 }
